1de99dd43a71c6d6f83608cb07f931eae0b93aec
salmi3d <salmi3d@gmail.com>
2020-02-20 16:46:47 +0200

    added home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>Home</div>
+  <div>
+    <div class="page-title">
+      <h3>Счет</h3>
+
+      <button class="btn waves-effect waves-light btn-small">
+        <i class="material-icons">refresh</i>
+      </button>
+    </div>
+    <div class="row">
+      <HomeBill />
+
+      <HomeCurrency />
+    </div>
+  </div>
 </template>
 
 <script>
+import HomeBill from '@/components/HomeBill'
+import HomeCurrency from '@/components/HomeCurrency'
+
 export default {
   name: 'home',
-  components: {}
+  components: {
+    HomeBill,
+    HomeCurrency,
+  }
 }
 </script>
